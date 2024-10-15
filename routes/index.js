d469d8f51df28cea44d2e1690eb5059f9f1d77f0
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-15 22:40:33 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const taskApi = require('./task.api');
 const userApi = require('./user.api');
 //묶어줄수 있어용 유알엘
-router.use('/task', taskApi);
+router.use('/task',taskApi);
 router.use('/user',userApi);
 // router.use('/ta')
 module.exports = router;
