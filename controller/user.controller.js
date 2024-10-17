11c14dcd3d6e7d85faded6c383b1f8bbd4d55920
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-17 17:42:35 +0900

    adduser

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,8 +8,9 @@ userController.createUser = async (req, res) => {
         const { email, name, password } = req.body;
         const user = await User.findOne({ email });
         if (user) {
-            throw new Error('이미가입이 된 유저 입니다. ');
+            throw new Error('You are already a registered user. ');
         }
+
         //암호화
         const salt = bcrypt.genSaltSync(saltRounds);
         const hash = bcrypt.hashSync(password, salt);
@@ -31,22 +32,25 @@ userController.loginWithEmail = async (req, res) => {
         // 4. 이 유전에 디비에 있는 패스워드와 프론트엔드가 보낸 패스워드가 같은지 비교
         // 5. 맞다 ! 토큰 발행
         // 6. 틀리면 에러베세지 보냄
-        // 응ㅇ답
+        // 응답
         const { email, password } = req.body;
         const user = await User.findOne({ email }, '-createdAt -updatedAt -__v');
         if (!user) {
             throw new Error('User not found');
         }
+
         const isMatch = bcrypt.compareSync(password, user.password);
         // password ==> 유저가 입력한 그자체
         // user.password=>암호화 된 패스워들 비교
         if (!isMatch) {
-            const token = await user.generateToken();
-            console.log(token);
-
-            return res.status(200).json({ status: 'success', user, token });
+            // Incorrect password or email
+            throw new Error('Email or password do not match');
         }
-        throw new Error('id or password not match');
+
+        const token = await user.generateToken();
+        // console.log(token);
+
+        return res.status(200).json({ status: 'success', user, token });
     } catch (error) {
         res.status(400).json({ status: 'fail', error: error.message });
     }
