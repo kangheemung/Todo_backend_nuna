9df2e6e08d1e2b8ac3ef65808d7a4459a2816645
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-18 13:48:53 +0900

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -35,24 +35,20 @@ userController.loginWithEmail = async (req, res) => {
         // 응답
         const { email, password } = req.body;
         const user = await User.findOne({ email }, '-createdAt -updatedAt -__v');
-        if (!user) {
-            throw new Error('User not found');
+        if (user) {
+            const isMatch = bcrypt.compareSync(password, user.password);
+            // password ==> 유저가 입력한 그자체
+            // user.password=>암호화 된 패스워들 비교
+            if (isMatch) {
+                const token = await user.generateToken();
+                // console.log(token);
+
+                return res.status(200).json({ status: 'success', user, token });
+            }
         }
-
-        const isMatch = bcrypt.compareSync(password, user.password);
-        // password ==> 유저가 입력한 그자체
-        // user.password=>암호화 된 패스워들 비교
-        if (!isMatch) {
-            // Incorrect password or email
-            throw new Error('Email or password do not match');
-        }
-
-        const token = await user.generateToken();
-        // console.log(token);
-
-        return res.status(200).json({ status: 'success', user, token });
+        throw new Error('Email or password does not match.');
     } catch (error) {
-        res.status(400).json({ status: 'fail', error: error.message });
+        res.status(400).json({ status: 'fail', message: error.message });
     }
 };
 module.exports = userController;
